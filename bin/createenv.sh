6e8454ae91e584f283108686e4a1fea56107253a
Tilman Kamp <5991088+tilmankamp@users.noreply.github.com>
2019-06-20 20:13:24 +0200

    Initial stucture

---
+++ bin/createenv.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+python3 -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
---
+++ bin/getmodel.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+version="0.5.0"
+dir="deepspeech-${version}-models"
+archive="${dir}.tar.gz"
+
+mkdir -p models
+cd models
+if [[ ! -f $archive ]] ; then
+    wget "https://github.com/mozilla/DeepSpeech/releases/download/v${version}/${archive}"
+fi
+
+tar -xzvf $archive
+mv $dir en
