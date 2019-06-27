c1c222c2dba0009334aaa92a151e66cc9323581d
Tilman Kamp <5991088+tilmankamp@users.noreply.github.com>
2019-06-27 23:52:45 +0200

    WIP

--- align/align.py
+++ align/align.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import text
 import logging
 import argparse
 import numpy as np
@@ -7,9 +8,11 @@ import wavTranscriber
 
 
 def main(args):
-    parser = argparse.ArgumentParser(description='Transcribe long audio files using webRTC VAD or use the streaming interface')
+    parser = argparse.ArgumentParser(description='Force align speech data with a transcript.')
     parser.add_argument('audio', type=str,
-                        help='Path to the audio file to run (WAV format)')
+                        help='Path to speech audio (WAV format)')
+    parser.add_argument('transcript', type=str,
+                        help='Path to original transcript')
     parser.add_argument('--aggressive', type=int, choices=range(4), required=False,
                         help='Determines how aggressive filtering out non-speech is. (Interger between 0-3)')
     parser.add_argument('--model', required=False,
@@ -25,6 +28,7 @@ def main(args):
     model_dir = os.path.expanduser(args.model if args.model else 'models/en')
     output_graph, alphabet, lm, trie = wavTranscriber.resolve_models(model_dir)
     model, _, _ = wavTranscriber.load_model(output_graph, alphabet, lm, trie)
+    alphabet = text.Alphabet(alphabet)
 
     inference_time = 0.0
 
@@ -32,21 +36,31 @@ def main(args):
     wave_file = args.audio
     aggressiveness = int(args.aggressive) if args.aggressive else 3
     segments, sample_rate, audio_length = wavTranscriber.vad_segment_generator(wave_file, aggressiveness)
-    f = open(wave_file.rstrip(".wav") + ".txt", 'w')
-    logging.debug("Saving Transcript @: %s" % wave_file.rstrip(".wav") + ".txt")
+
+    with open(args.transcript, 'r') as transcript_file:
+        original_transcript = transcript_file.read()
+    original_transcript = ' '.join(original_transcript.lower().split())
+    original_transcript = alphabet.filter(original_transcript)
+
+    position = 0
 
     for i, segment in enumerate(segments):
         # Run DeepSpeech on the chunk that just completed VAD
-        logging.debug("Processing chunk %002d" % (i,))
+        logging.debug("Transcribing segment %002d..." % i)
         audio = np.frombuffer(segment, dtype=np.int16)
-        transcript, segment_inference_time = wavTranscriber.stt(model, audio, sample_rate)
+        segment_transcript, segment_inference_time = wavTranscriber.stt(model, audio, sample_rate)
         inference_time += segment_inference_time
-        logging.info("Transcript: %s" % transcript)
 
-        f.write(transcript + " ")
+        logging.debug("Looking for segment transcript in original transcript...")
+        distance, found_offset, found_len = \
+            text.minimal_distance(original_transcript,
+                                  segment_transcript,
+                                  start=position,
+                                  threshold=0.1)
+        logging.info("Segment transcript: %s" % segment_transcript)
+        logging.info("Segment      found: %s" % original_transcript[found_offset:found_offset+found_len])
+        logging.info("--")
 
-    # Summary of the files processed
-    f.close()
 
 if __name__ == '__main__':
     main(sys.argv[1:])
--- align/text.py
+++ align/text.py
@@ -1,9 +1,7 @@
 from __future__ import absolute_import, division, print_function
 
 import codecs
-import re
-
-import numpy as np
+import logging
 
 from six.moves import range
 
@@ -19,12 +17,22 @@ class Alphabet(object):
                     line = '#\n'
                 elif line[0] == '#':
                     continue
-                self._label_to_str += line[:-1] # remove the line ending
+                self._label_to_str += line[:-1]  # remove the line ending
                 self._str_to_label[line[:-1]] = self._size
                 self._size += 1
 
     def string_from_label(self, label):
         return self._label_to_str[label]
+
+    def has_label(self, string):
+        return string in self._str_to_label
+
+    def filter(self, string):
+        new_string = ''
+        for c in string:
+            if self.has_label(c):
+                new_string += c
+        return new_string
 
     def label_from_string(self, string):
         try:
@@ -47,31 +55,6 @@ class Alphabet(object):
         return self._config_file
 
 
-def text_to_char_array(original, alphabet):
-    """
-    Given a Python string ``original``, remove unsupported characters, map characters
-    to integers and return a numpy array representing the processed string.
-    """
-    return np.asarray([alphabet.label_from_string(c) for c in original])
-
-
-# Validate and normalize transcriptions. Returns a cleaned version of the label
-# or None if it's invalid.
-def validate_label(label):
-    # For now we can only handle [a-z ']
-    if re.search(r"[0-9]|[(<\[\]&*{]", label) is not None:
-        return None
-
-    label = label.replace("-", "")
-    label = label.replace("_", "")
-    label = label.replace(".", "")
-    label = label.replace(",", "")
-    label = label.replace("?", "")
-    label = label.replace("\"", "")
-    label = label.strip()
-    label = label.lower()
-
-    return label if label else None
 
 
 # The following code is from: http://hetland.org/coding/python/levenshtein.py
@@ -105,3 +88,25 @@ def levenshtein(a, b):
             current[j] = min(add, delete, change)
 
     return current[n]
+
+
+def minimal_distance(search_in, search_for, start=0, threshold=0):
+    best_distance = 1000000000
+    best_offset = -1
+    best_len = -1
+    window = 10
+    rough_acceptable_distance = int(1.5 * window)
+    acceptable_distance = int(len(search_for) * threshold)
+    stop = len(search_in)-len(search_for)
+    for rough_offset in range(start, stop, window):
+        rough_distance = levenshtein(search_in[rough_offset:rough_offset+len(search_for)], search_for)
+        if rough_distance < rough_acceptable_distance:
+            for offset in range(rough_offset-window, rough_offset+window, 1):
+                distance = levenshtein(search_in[offset:offset+len(search_for)], search_for)
+                if distance < best_distance:
+                    best_distance = distance
+                    best_offset = offset
+                    best_len = len(search_for)
+            if best_distance <= acceptable_distance:
+                return best_distance, best_offset, best_len
+    return -1, 0, 0
