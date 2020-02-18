02305706d0592c920dbd59278304d2f5209ffc04
Tilman Kamp <5991088+tilmankamp@users.noreply.github.com>
2020-02-18 17:28:11 +0100

    Remove incompatible stty sane call

--- bin/align.sh
+++ bin/align.sh
@@ -2,4 +2,3 @@
 approot=$(cd "$(dirname "$(dirname "$0")")" && pwd)
 source "$approot/venv/bin/activate"
 python "$approot/align/align.py" "$@"
-stty sane
--- bin/catalog_tool.sh
+++ bin/catalog_tool.sh
@@ -2,4 +2,3 @@
 approot=$(cd "$(dirname "$(dirname "$0")")" && pwd)
 source "$approot/venv/bin/activate"
 python "$approot/align/catalog_tool.py" "$@"
-stty sane
